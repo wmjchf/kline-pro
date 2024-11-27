3bc4e127f46f4eb7243d855e9f9e87bc4ef3ea57
wmjchf <3284304056@qq.com>
2024-11-27 22:04:50 +0800

    feat: 打包配置更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,37 +1,37 @@
 /// <reference types="vite/client" />
 
-import { defineConfig } from 'vite'
-import solidPlugin from 'vite-plugin-solid'
+import { defineConfig } from "vite";
+import solidPlugin from "vite-plugin-solid";
 
 export default defineConfig({
   plugins: [solidPlugin()],
   build: {
-    cssTarget: 'chrome61',
+    cssTarget: "chrome61",
     sourcemap: true,
     rollupOptions: {
-      external: ['klinecharts'],
+      external: ["klinecharts"],
       output: {
         assetFileNames: (chunkInfo) => {
-          if (chunkInfo.name === 'style.css') {
-            return 'klinecharts-pro.css'
+          if (chunkInfo.name === "style.css") {
+            return "kline-pro.css";
           }
         },
         globals: {
-          klinecharts: 'klinecharts'
+          klinecharts: "kline",
         },
       },
     },
     lib: {
-      entry: './src/index.ts',
-      name: 'klinechartspro',
+      entry: "./src/index.ts",
+      name: "kline-pro",
       fileName: (format) => {
-        if (format === 'es') {
-          return 'klinecharts-pro.js'
+        if (format === "es") {
+          return "kline-pro.js";
         }
-        if (format === 'umd') {
-          return 'klinecharts-pro.umd.js'
+        if (format === "umd") {
+          return "kline-pro.umd.js";
         }
-      }
-    }
-  }
-})
+      },
+    },
+  },
+});
